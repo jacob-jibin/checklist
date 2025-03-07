df2c0ae4b00be794740f126875ea561cc4f6913a
Sreenesh Pai <srpai@eg.dk>
2025-03-07 17:11:32 +0530

    update multiple choice item test

--- Engine.Tests/Unit/MultipleChoiceItemTest.cs
+++ Engine.Tests/Unit/MultipleChoiceItemTest.cs
@@ -18,7 +18,7 @@ namespace Engine.Tests.Unit
         [Fact]
         public void SingleItem()
         {
-            var item = new MultipleChoiceItem(RedCarpet, GreenCarpet, NoCarpet);
+            var item = "Which Carpet Color?".Choices(RedCarpet, GreenCarpet, NoCarpet);
             var checklist = new Checklist(creator, item);
             Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
             creator.Sets(item).To(GreenCarpet);
@@ -31,18 +31,19 @@ namespace Engine.Tests.Unit
         [Fact]
         public void EmptyChecklist()
         {
-            var item = new MultipleChoiceItem(RedCarpet);
+            var item = "Which Carpet Color?".Choices(RedCarpet);
             var checklist = new Checklist(creator, item);
             Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
             creator.Cancel(item).In(checklist);
             Assert.Equal(ChecklistStatus.NotApplicable, checklist.Status());
         }
+
         [Fact]
         public void MixedItems()
         {
-            var item1 = new MultipleChoiceItem(RedCarpet, GreenCarpet, NoCarpet);
-            var item2 = new BooleanItem();
-            var item3 = new MultipleChoiceItem("India", "Iceland", "Norway");
+            var item1 = "Which Carpet Color?".Choices(RedCarpet, GreenCarpet, NoCarpet);
+            var item2 = ("Is US citizen?").TrueFalse();
+            var item3 = "Which country?".Choices("India", "Iceland", "Norway");
             var checklist = new Checklist(creator, item1, item2, item3);
 
             Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
@@ -52,6 +53,14 @@ namespace Engine.Tests.Unit
             Assert.Equal(ChecklistStatus.Succeeded, checklist.Status());
             creator.Sets(item3).To("Poland");
             Assert.Equal(ChecklistStatus.Failed, checklist.Status());
+
+            var answers = new CurrentAnswers(checklist);
+
+            Assert.Equal(GreenCarpet, answers.value("Which Carpet Color?"));
+            Assert.Equal("Poland", answers.value("Which country?"));
+            Assert.Equal(true, answers.value("Is US citizen?"));
+            //null
+            //throws
         }
 
         [Fact]
